a8bef03129596e5d8ff1ca54831a7f574a6322dd
Aleksandr12354 <super.email15243@gmail.com>
2025-01-28 17:53:34 +0300

    weapon fix and weapon class docstrings

--- weapon_class.py
+++ weapon_class.py
@@ -3,14 +3,27 @@ from constants import *
 
 # Класс оружия, позволяющий создавать объекты, хранящие в себе характеристики оружия
 class Weapon:
-    def __init__(self, name, player_speed_mul=1, cooldown_ticks=None,
-                 bullet_speed_mul=1, blt_size=(10,5), bullet_dmg=100, bullet_number=1, crit_chance=20, bullet_clr='Blue'):
+    def __init__(self, name, player_speed_mul=1,
+                 bullet_speed_mul=1, blt_size=(10,5), bullet_dmg=100, bullet_number=1, bullet_clr='Blue'):
+
+        """
+        Инициализация класса оружия
+
+        :param name -- название оружия:
+        :param player_speed_mul: -- коэффицент, с которым оружие увеличивает или уменьшает начальную скорость игрока
+        :param bullet_speed_mul: -- коэффицент, увеличиваюзий или уменьшающий скорость пули оружия
+        :param blt_size: -- кортеж с размером пули (x, y)
+        :param bullet_dmg: -- урон, который пуля оружия наносит вргагм
+        :param bullet_number: -- количество пуль, появляющихся после одного выстрела
+        :param bullet_clr: -- цвет пуль оружия
+
+        :return: None
+        """
+
         self.name = name
         self.plr_spd = player_speed_mul * PLAYER_SPEED
-        self.cooldown = cooldown_ticks
         self.blt_spd = bullet_speed_mul * BULLET_SPEED
         self.blt_size = blt_size
         self.blt_dmg = bullet_dmg
         self.blt_n = bullet_number
-        self.crit_chance = crit_chance
         self.blt_clr = bullet_clr
--- weapon_inventory.py
+++ weapon_inventory.py
@@ -4,30 +4,30 @@ from weapon_class import Weapon
 # Типы оружия игрока:
 
 # игрок не держит оружия, не стреляет т.к. кол-во пуль = 0
-nothing = Weapon('Nothing',1,0,
+nothing = Weapon('Nothing',1,
                  0, (1, 1),0,
-                 0, 0, 'Black')
+                 0, 'Black')
 
-pistol = Weapon('Pistol', 1, 1,
+pistol = Weapon('Pistol', 1,
                 2, (8, 4),40,
                 1, 20, 'Orange')
 
-rifle_semi = Weapon('Rifle (Semi)', 0.5, 10,
+rifle_semi = Weapon('Rifle (Semi)', 0.5,
                4, (10, 5), 80,
-                1, 70, 'Blue')
+                1, 'Blue')
 
-rifle_burst = Weapon('Rifle (Burst)', 0.5, 15,
+rifle_burst = Weapon('Rifle (Burst)', 0.5,
                4, (10, 5), 40,
-                3, 70, 'Purple')
+                3, 'Purple')
 
 # Инвентарь игрока:
-inventory = [nothing,   # 0
-             pistol,    # 1
-             rifle_semi,     # 2
+inventory = [nothing,       # 0
+             pistol,        # 1
+             rifle_semi,    # 2
              rifle_burst,   # 3
-             nothing,   # 4
-             nothing,   # 5
-             nothing,   # 6
-             nothing,   # 7
-             nothing,   # 8
-             nothing]   # 9
+             nothing,       # 4
+             nothing,       # 5
+             nothing,       # 6
+             nothing,       # 7
+             nothing,       # 8
+             nothing]       # 9
